6f8849cb396c84f893a77bb3fbc1804029ae781d
sssmilar <realdeadinst@gmail.com>
2025-06-24 00:19:27 +0300

    переделал sql запросы3

---
+++ next.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'githubusercontent.com',
+      },
+      // Добавь другие домены, если используешь их для картинок
+    ],
+  },
+}; 
